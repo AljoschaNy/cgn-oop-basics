fdcb26ef30932866ac21adab750971abdf56e3b6
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-26 11:51:31 +0200

    Add accelerateCar method to Car class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,14 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Car myCar = new Car("BMW","3.20","blue",2018);
+        Car myCar2 = new Car("Dacia", "Sandero","blue",2020);
+
+        System.out.println("Speed before = " + myCar2.speedInKNH);
+        myCar2.accelerateCar();
+        System.out.println("Speed after = " + myCar2.speedInKNH);
+
+
+
 
         myCar.startCar();
     }
