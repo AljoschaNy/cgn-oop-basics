00c687395067c940431160e548a03ee303196361
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-26 12:00:51 +0200

    Add Person Instances

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,17 @@ public class Main {
         Car myCar = new Car("BMW","3.20","blue",2018);
         Car myCar2 = new Car("Dacia", "Sandero","blue",2020);
 
-        System.out.println("Speed before = " + myCar2.speedInKNH);
-        myCar2.accelerateCar();
-        System.out.println("Speed after = " + myCar2.speedInKNH);
+        //System.out.println("Speed before = " + myCar2.speedInKNH);
+        //myCar2.accelerateCar();
+        //System.out.println("Speed after = " + myCar2.speedInKNH);
+        //myCar.startCar();
 
+        Person personAljoscha = new Person("Aljoscha",32,"male");
+        Person personIskren = new Person("Iskren", 32, "male");
+        Person personAlex = new Person("Alexander",16, "unknown");
 
-
-
-        myCar.startCar();
+        personAljoscha.greeting();
+        personAlex.greeting();
+        personIskren.greeting();
     }
 }
